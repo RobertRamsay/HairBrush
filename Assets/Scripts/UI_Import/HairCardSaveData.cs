ad218b663a928daa6e2d15ce1b3dab1c9f863ca9
Robert Ramsay <me@robertramsay.co.uk>
2026-08-12 16:11:56 +0100

    Store current-format card placement data directly

--- Assets/Scripts/UI_Import/HairCardSaveData.cs
+++ Assets/Scripts/UI_Import/HairCardSaveData.cs
@@ -2,7 +2,28 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable] public class HairCardSaveData { public float posX,posY,posZ; public float rotX,rotY,rotZ,rotW; public float length; public float width; public int segments; public float bendAngle; public float twistAngle; public float flattenFactor; public float embedDepth; public float offsetX,offsetY,offsetZ; public float uScale; public float vScale; public float uOffset; public float vOffset; public int groupId; }
+[Serializable]
+public class HairCardSaveData
+{
+    public float posX,posY,posZ;
+    public float rotX,rotY,rotZ,rotW;
+    public float hitX,hitY,hitZ;
+    public float normalX,normalY,normalZ;
+    public float length;
+    public float width;
+    public int segments;
+    public float bendAngle;
+    public float twistAngle;
+    public float flattenFactor;
+    public float embedDepth;
+    public float offsetX,offsetY,offsetZ;
+    public float uScale;
+    public float vScale;
+    public float uOffset;
+    public float vOffset;
+    public int groupId;
+}
+
 [Serializable] public class VarianceChannelSaveData { public string channel; public float amount; public int seed; }
 [Serializable] public class ClumpPointSaveData { public float posX,posY,posZ; public float normalX,normalY,normalZ; public float strength; }
 [Serializable] public class ClumpLayerSaveData { public bool enabled; public int pointCount=20; public int generationSeed; public float globalStrength=1f; public float brushRadius=.08f; public float brushStrength=.5f; public float brushFalloff=.5f; public float brushValue=1f; public int debugMode; public float curveEarly=.08f; public float curveMid=.65f; public float curveTip=1f; public List<ClumpPointSaveData> points=new(); }
@@ -44,8 +65,21 @@ public class HairProjectSaveData : ISerializationCallbackReceiver
 {
     public static HairProjectSaveData PendingModifierRestore;
     public string modelPath;
-    public List<GroupSaveData> groups=new(); public List<HairCardSaveData> hairCards=new();
-    public float sliderLength; public float sliderWidth; public int sliderSegments; public float sliderBend; public float sliderTwist; public float sliderEmbedDepth; public float sliderOffsetX; public float sliderOffsetY; public float sliderOffsetZ; public float sliderUScale; public float sliderVScale; public float sliderUOffset; public float sliderVOffset;
+    public List<GroupSaveData> groups=new();
+    public List<HairCardSaveData> hairCards=new();
+    public float sliderLength;
+    public float sliderWidth;
+    public int sliderSegments;
+    public float sliderBend;
+    public float sliderTwist;
+    public float sliderEmbedDepth;
+    public float sliderOffsetX;
+    public float sliderOffsetY;
+    public float sliderOffsetZ;
+    public float sliderUScale;
+    public float sliderVScale;
+    public float sliderUOffset;
+    public float sliderVOffset;
 
     public void OnBeforeSerialize()
     {
@@ -56,8 +90,6 @@ public class HairProjectSaveData : ISerializationCallbackReceiver
 
     public void OnAfterDeserialize()
     {
-        // ModelViewer still has to recreate the model/cards first. The bridge restores
-        // modifier state on the next safe runtime update after those cards exist.
         PendingModifierRestore=this;
     }
 }
